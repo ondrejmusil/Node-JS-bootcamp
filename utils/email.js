37b0378e2595764e1b17ea2911b2978ed2e77204
Ondrej Musil <>
2023-07-25 15:08:25 +0200

    Minor update

--- utils/email.js
+++ utils/email.js
@@ -13,7 +13,7 @@ const sendEmail = async options => {
 
     // 2) Define email options
     const mailOptions = {
-        from: 'Custom testing service <testingService@blablagg.com>',
+        from: 'Custom testing service <testingService@testing.com>',
         to: options.email,
         subject: options.subject,
         text: options.message
